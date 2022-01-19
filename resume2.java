6c3f29ee94791c6a022f655c1236b8efc5433f00
praveshtiwari701 <90543828+praveshtiwari701@users.noreply.github.com>
2022-01-19 15:58:24 +0530

    Add files via upload

---
+++ resume2.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+
+class resume2{
+  public static void about(){
+    System.out.println("-------------------------------------RESUME-----------------------------------");
+    System.out.println("");
+    System.out.println("Name: Pravesh Tiwari");
+    System.out.println("Contact: 12345678");
+    System.out.println("Father Name: Vimlesh Tiwari");
+    System.out.println("Date of Birth: 02/02/2002");
+    System.out.println("Gender:  Male");
+    System.out.println("Religion: Hindu");
+    System.out.println("Nationality: Indian");
+    System.out.println("E-mail: praveshtiwari@gmail.com");
+    System.out.println("");
+    
+
+  }
+ 
+  public static void education(){
+    System.out.println("------------------------------------EDUCATIONAL QUALIFICATION--------------------------"); 
+    System.out.println("");
+    System.out.println("1.UITBU");
+    System.out.println("B.E");
+    System.out.println("2020-Present");
+    System.out.println("");
+    System.out.println("2.ST JOSEPH'S PU COLLEGE ");
+    System.out.println("Class 12");
+    System.out.println("2018-20");
+    System.out.println("");
+    System.out.println("3.SRI VINAYAKA HIGH SCHOOL");
+    System.out.println("Class 10");
+    System.out.println("2008-18");
+    System.out.println("");
+    System.out.println("");
+      
+      
+  }
+  public static void skills(){
+    System.out.println("------------------------------------SKILLS KNOWN----------------------------------------");
+        
+    System.out.println("");
+    System.out.println("1.C");
+    System.out.println("2.C++ Programming Language");
+    System.out.println("3.Java Programming Language");
+    System.out.println("4.Python");
+    System.out.println("6.HTML");
+    System.out.println("7.CSS");
+    System.out.println("8.Java Script");
+    System.out.println("9.Node js");
+    System.out.println("10.DBMS");
+    System.out.println("");
+    System.out.println("");
+  }
+  
+  public static void projects(){
+    System.out.println("---------------------------------PROJECT----------------------------------------");
+    
+    System.out.println("");
+    System.out.println("1.Library Management system :Using C Programming");
+    System.out.println("");
+    System.out.println("2.A Website For A Local Gym: Using HTML, CSS");
+    System.out.println("");
+    System.out.println("");
+  }
+  public static void signature(){
+    System.out.println("---------------------------------DECLARATION------------------------------------");
+    
+    System.out.println("");
+    System.out.println("I do hereby declare that the information given in this document are true to the best of my knowledge and belief");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("SIGNATURE: ");
+    System.out.println("");
+  }
+  public static void main(String args[]){
+      
+    about();
+    education();
+    skills();
+    projects();
+    signature();
+  }
+}
